5dd56dfcdfd44fbb0b2b1b06f98015c3bfbceb59
Marcus Silver <46176920+marcus11333@users.noreply.github.com>
2024-04-28 17:22:08 +0000

    added logarithmic coloring, gamma correction, and more

--- Coding/PROJECTS/flame/script.js
+++ Coding/PROJECTS/flame/script.js
@@ -71,24 +71,22 @@ function ex (x,y){
   j0 = j0 * j0 * j0
   return [r * (i0 + j0), r * (i0 - j0)]
 }
-//
+function cylinder (x,y){
+  return [Math.sin(x), y]
+}
+
 //number of functions to choose from
 const j = 7
+
 //canvas resolution
-const res = 600
+const res = 700;
+
 //list of all possible functions
-//
+const FUNCTIONS = [linear,sinusoidal,spherical,swirl,horseshoe,polar,spiral,hyperbolic,diamond,julia,handkerchief,disk,eyefish,ex,cylinder] //noise
 
-
-
-const FUNCTIONS = [linear,sinusoidal,spherical,swirl,horseshoe,polar,spiral,hyperbolic,diamond,julia,noise,handkerchief,disk,eyefish,ex]
 //list of selected functions
 FUNCS = Array.from({length: j}, () => FUNCTIONS[Math.floor(Math.random() * FUNCTIONS.length)] );
 
-
-//
-//
-//
 //canvas + miscellaneous variables
 const tau = 2 * Math.PI
 const res2 = res/2
@@ -96,118 +94,78 @@ const canvas = document.getElementById("canv");
 const ctx = canvas.getContext("2d");
 canvas.width = res.toString();
 canvas.height = res.toString();
-//
+
+var imageData = ctx.getImageData(0, 0, res, res);
+var data = imageData.data;
+
 //fill canvas black
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, res, res);
-//
-//draw pixel to canvas
-function draw2canvas(xPos, yPos, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(xPos, yPos, 1, 1);
-}
+
+
 //numbers
-const aj = Array.from({length: j}, () => Math.random() );
-const bj = Array.from({length: j}, () => Math.random() );
-const cj = Array.from({length: j}, () => Math.random() -.5 );
-const dj = Array.from({length: j}, () => Math.random() );
-const ej = Array.from({length: j}, () => Math.random() );
-const fj = Array.from({length: j}, () => Math.random() -.5 );
-//
+const a_j = Array.from({length: j}, () => 2*Math.random()-1);
+const b_j = Array.from({length: j}, () => 2*Math.random()-1);
+const c_j = Array.from({length: j}, () => 2*Math.random()-1);
+const d_j = Array.from({length: j}, () => 2*Math.random()-1);
+const e_j = Array.from({length: j}, () => 2*Math.random()-1);
+const f_j = Array.from({length: j}, () => 2*Math.random()-1);
+
 //fractal flame function
 function F(x,y){
-  //index of function in FUNCTIONS list
+  //index for function
   const rand = Math.floor(j *Math.random()) 
-  const X = aj[rand] * x + bj[rand] * y + cj[rand]
-  const Y = dj[rand] * x + ej[rand] * y + fj[rand]
+  const X = a_j[rand] * x + b_j[rand] * y + c_j[rand]
+  const Y = d_j[rand] * x + e_j[rand] * y + f_j[rand]
   const P = FUNCS[rand](X,Y)
   return [P[0],P[1],rand]
 }
-//
+
 //Inigo Quillez coloring
 function Q(t,d) {
   const c = .5 + Math.cos(4 * tau * (t+d)) * .5;
-  return c
+  return (255 * c) //<< 0
 }
-//
-//
-// Histogram
-histogram = Array(res).fill().map(() => Array(res).fill(0));
-let MAX = 0
-RED = Array(res).fill().map(() => Array(res).fill(.5));
-GREEN = Array(res).fill().map(() => Array(res).fill(.5));
-BLUE = Array(res).fill().map(() => Array(res).fill(.5));
 
-//
-//
+data = data.fill(0)
+for (i in data){
+  if (i % 4 == 3) data[i] = 255
+}
+ctx.putImageData(imageData, 0, 0);
+histogram = Array(res*res).fill(0);
+max = 0
 
 function update(){
   let Px = 2 * Math.random() - 1
   let Py = 2 * Math.random() - 1
-  let Pred = 0
-  let Pgreen = 0
-  let Pblue = 0
-  for (let i = 0; i < 10000; i++){
+  for (let i = 0; i < 20; i++){
     const arr = F(Px,Py)
     Px = arr[0]
     Py = arr[1]
-    const temp = arr[2]/j
-    Pred = Pred/2 + Q(temp,0)/2
-    Pgreen = Pgreen/2 + Q(temp,1/3)/2
-    Pblue = Pblue/2 + Q(temp,2/3)/2
-
-    if (i >= 20){
-      if (Math.max(Math.abs(Px),Math.abs(Py)) < 1){
-        const X0 = Math.floor(res2 * Px + res2)
-        const Y0 = Math.floor(res2 * Py + res2)
-
-        histogram[X0][Y0]++
-        RED[X0][Y0] = (RED[X0][Y0] + Pred)/2
-        GREEN[X0][Y0] = (GREEN[X0][Y0] + Pgreen)/2
-        BLUE[X0][Y0] = (BLUE[X0][Y0] + Pblue)/2
-
-
-        //
-        if (histogram[X0][Y0] > MAX){
-          MAX = histogram[X0][Y0]
-        }
-
-        let co = Math.log(histogram[X0][Y0]) / Math.log(MAX) * 255
-        red = co * RED[X0][Y0]
-        green = co * GREEN[X0][Y0]
-        blue = co * BLUE[X0][Y0]
-        draw2canvas(X0,Y0,"rgb("+red+","+green+","+blue+")")
-      }
-
-    }
   }
+  for (let i = 20; i < 10000; i++){
+    const arr = F(Px,Py)
+    Px = arr[0]
+    Py = arr[1]
+    const temp = arr[2]/(j)
+    const index = ( ((res2 * Py + res2) << 0) * res + ((res2 * Px + res2) << 0)) * 4;
+    data[index] = (.9*data[index] + .1*Q(temp,0))
+    data[index+1] = (.9*data[index+1] + .1*Q(temp,.33333))
+    data[index+2] = (.9*data[index+2] + .1*Q(temp,.66666))
+    histogram[index/4] += 1
+    if (histogram[index/4] > max) max = histogram[index/4]
+    scl = (Math.log(histogram[index/4])/Math.log(max)) ** (1/2.2)
+    data[index] = (data[index]*scl) << 0
+    data[index+1] = (data[index+1]*scl) << 0
+    data[index+2] = (data[index+2]*scl) << 0
+  }
+  //
+  ctx.putImageData(imageData, 0, 0);
   requestAnimationFrame(update)
-
 }
 //
 //write function names on document
-document.getElementById("p").innerHTML = 'This program creates a random fractal flame using many different random functions, read more about fractal flames on the wikipedia page. <br><br> the functions used in the current fractal are: <br><br>'
 for (i of FUNCS ){
   document.getElementById("p").innerHTML = document.getElementById("p").innerHTML + " " + i.name
 }
 //
 //
 update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
