b8321d5bc6f85afcaa40eb6cc04db54e0ee55f0b
Marcus Silver <46176920+marcus11333@users.noreply.github.com>
2024-09-27 14:27:33 -0500

    Update script.js

--- Art/script.js
+++ Art/script.js
@@ -1,8 +1,17 @@
-path = "art_images/flames/"
+let path = "art_images/flames/"
 const flame_box = document.getElementById("flame_box");
 for (i=1;i<85;i++){
     let isrc = path + "flame" + i + ".jpg"
     let img = document.createElement("img")
     img.src = isrc
     flame_box.appendChild(img)
+}
+
+path = "art_images/fractals/"
+const fractal_box = document.getElementById("fractal_box");
+for (i=1;i<149;i++){
+    let isrc = path + "fractal" + i + ".jpg"
+    let img = document.createElement("img")
+    img.src = isrc
+    fractal_box.appendChild(img)
 }
